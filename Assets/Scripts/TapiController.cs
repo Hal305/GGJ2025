23b9a9bc0affc3bd207bd9700c0df2ef455545b0
Magnus <magnus.nistad.helvig@gmail.com>
2025-01-25 13:24:19 +0100

    Add scriptable object

--- Assets/Scripts/TapiController.cs
+++ Assets/Scripts/TapiController.cs
@@ -11,10 +11,41 @@ public class TapiController : MonoBehaviour
     private void Update()
     {
         transform.Translate(direction * speed * Time.deltaTime);
+        var colliders = Physics2D.OverlapCircleAll(transform.position, 0.5f);
+        int bubbleCounter = 0;
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].tag == "Tapioka")
+            {
+                if (colliders[i].GetComponent<TapiController>().variant == variant)
+                {
+                    bubbleCounter ++;
+                }
+            }
+        }
+        //If this is colliding with 2 or more other bubbles of the same color
+        if (bubbleCounter > 2)
+        {
+            //Ondestroy Pop nearby bubbles of the same color
+            Destroy(gameObject);
+        }
     }
 
     void OnCollisionStay2D(Collision2D collision)
     {
-        speed = 0f;
+        speed = 0.03f;
+        direction = Vector2.up;
+    }
+
+    private void OnDestroy()
+    {
+        var colliders = Physics2D.OverlapCircleAll(transform.position, 0.6f);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].tag == "Tapioka")
+            {
+                if (colliders[i].GetComponent<TapiController>().variant == variant) Destroy(colliders[i].gameObject);
+            }
+        }
     }
 }
