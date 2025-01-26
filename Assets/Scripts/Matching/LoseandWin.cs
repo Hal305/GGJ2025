9e04790d987d41ca46445254db84f3c4d334a9bf
Magnus <magnus.nistad.helvig@gmail.com>
2025-01-26 14:06:01 +0100

    Matching Transition

--- Assets/Scripts/Matching/LoseandWin.cs
+++ Assets/Scripts/Matching/LoseandWin.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 public class LoseandWin : MonoBehaviour
 {
     private bool end = false;
+    private float winTime = 60f;
+    
+    
+    
     void OnTriggerStay2D(Collider2D other)
     {
         if (other.tag == "Tapioka" && !end)
         {
             //Scene Transition to menu
+            TransitionUI.Instance.PlayTransition(0);
             print("Lost");
             end = true;
         }
@@ -16,9 +21,10 @@ public class LoseandWin : MonoBehaviour
 
     void Update()
     {
-        if (Time.timeSinceLevelLoad > 60f && !end)
+        if (Time.timeSinceLevelLoad > winTime && !end)
         {
             //Scene transition to machine
+            TransitionUI.Instance.PlayTransition(1);
             print("Win");
             end = true;
         }
